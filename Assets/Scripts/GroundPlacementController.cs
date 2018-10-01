dcc5483903b80830ca0ebe19fae0cebde5a1427b
JoeVRLab <chatelainj19@knoxr1.us>
2018-10-01 15:10:23 -0500

    Final Push 10/1

---
+++ Assets/Scripts/GroundPlacementController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundPlacementController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject placeableObjectPrefab();
+
+    [SerializeField]
+    private KeyCode placeableObjectHotKey = KeyCode.A;
+
+    private GameObject currentPlaceableObject();
+
+    private void Update()
+    {
+        if (currentPlaceableObject != null)
+        {
+            MoveCurrentPlaceableObjectToMouse
+        }
+    }
+
+    private void MoveCurrentPlaceableObjectToMouse()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HandleNewObjectHotKey()
+
+    {
+       if {Input.GetKeyDown(HandleNewObjectHotKey)}
+        {
+            if (currentPlaceableObject == null)
+            {
+                currentPlaceableObject = Instatiate(placeableObjectPrefab);
+            }
+            else
+            {
+                Destroy(currentPlaceableObject);
+            }
+        }
+	}
+}
