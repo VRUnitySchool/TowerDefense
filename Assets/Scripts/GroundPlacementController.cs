443464279073afc1c9ce32d063efbdea5a8dda09
JoeVRLab <chatelainj19@knoxr1.us>
2018-10-02 14:48:45 -0500

    Fixed GroundPlacement

--- Assets/Scripts/GroundPlacementController.cs
+++ Assets/Scripts/GroundPlacementController.cs
@@ -1,43 +1,58 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GroundPlacementController : MonoBehaviour {
 
     [SerializeField]
-    private GameObject placeableObjectPrefab();
+    private GameObject placeableObjectPrefab;
 
     [SerializeField]
-    private KeyCode placeableObjectHotKey = KeyCode.A;
+    private KeyCode newObjectHotKey = KeyCode.A;
 
-    private GameObject currentPlaceableObject();
+    private GameObject currentPlaceableObject;
 
     private void Update()
     {
         if (currentPlaceableObject != null)
         {
-            MoveCurrentPlaceableObjectToMouse
+            MoveCurrentPlaceableObjectToMouse();
+            ReleaseIfClicked();
+        }
+    }
+
+    private void ReleaseIfClicked()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            currentPlaceableObject = null;
         }
     }
 
     private void MoveCurrentPlaceableObjectToMouse()
     {
-        throw new NotImplementedException();
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitinfo;
+        if (Physics.Raycast(ray, out hitinfo))
+        {
+            currentPlaceableObject.transform.position = hitinfo.point;
+            currentPlaceableObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitinfo.normal);
+        }
     }
 
     private void HandleNewObjectHotKey()
 
     {
-       if {Input.GetKeyDown(HandleNewObjectHotKey)}
-        {
+        if  (Input.GetKeyDown(newObjectHotKey))
             if (currentPlaceableObject == null)
             {
-                currentPlaceableObject = Instatiate(placeableObjectPrefab);
+                currentPlaceableObject = Instantiate(placeableObjectPrefab);
             }
             else
             {
                 Destroy(currentPlaceableObject);
             }
         }
-	}
 }
