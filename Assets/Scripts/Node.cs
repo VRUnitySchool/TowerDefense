15afe7c0e6a5e68e7d47017397a87a01e258a36a
NickVRLab <scottn19@knoxr1.us>
2018-10-02 14:57:38 -0500

    BuildManager/Node Scripts/GameMaster

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    private GameObject turret;
+
+
+    void OnMouseDown()
+    {
+        if (turret != null)
+        {
+            Debug.Log("Can't Build There");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position, transform.rotation);
+    }
+}
