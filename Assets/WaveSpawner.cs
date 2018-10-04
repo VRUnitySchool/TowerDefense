be78c9cb026376c608b2cb638cecd477e6fd6f7c
NickVRLab <scottn19@knoxr1.us>
2018-10-04 16:07:16 -0500

    HELP ME PLZ

--- Assets/WaveSpawner.cs
+++ Assets/WaveSpawner.cs
@@ -1,9 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour {
 
+    public Transform enemyPrefab;
 
+    public Transform spawnPoint;
 
+    public float timeBetweenWaves = 5f;
+    private float countdown= 2f;
+
+    private int waveNumber = 1;
+
+    void Update()
+    {
+        if (countdown <= 0)
+        {
+            SpawnWave();
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+    }
+
+    void SpawnWave()
+    {
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+        }
+
+        waveNumber++;
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
 }
