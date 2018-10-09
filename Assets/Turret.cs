44e4c9151b56df88debc7260c2958e9c15aa2309
NickVRLab <scottn19@knoxr1.us>
2018-10-09 14:38:28 -0500

    Turret rotation/Moved Turret prefab(easier for me to test)

--- Assets/Turret.cs
+++ Assets/Turret.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour {
 
-    public Transform target;
+    private Transform target;
     public float range = 15f;
 
     public string enemyTag = "Enemy";
+
+    public Transform partToRotate;
+    public float turnSpeed = 10f;
 
     void Start()
     {
@@ -42,9 +45,14 @@ public class Turret : MonoBehaviour {
     void Update()
     {
         if (target == null)
-        {
             return;
-        }
+
+        //Target Lock On
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
     }
 
     void OnDrawGizmosSelected()
