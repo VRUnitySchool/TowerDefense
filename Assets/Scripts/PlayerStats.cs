99321a92ca33c877ce0c92ec2f2300698c3a1a2b
NickVRLab <scottn19@knoxr1.us>
2018-10-12 14:09:45 -0500

    Turret/PlayerStats Script

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+    public static int Lives;
+    public int startLives = 20;
+
+    void Start () {
+
+        Lives = startLives;
+
+	}
+}
