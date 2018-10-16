a47f863961823859dab61cff6dc30b99bf47e791
NickVRLab <scottn19@knoxr1.us>
2018-10-16 14:57:24 -0500

    Cleaned up enemy movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,6 +3,9 @@ using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour {
 
+    public float startSpeed = 10f;
+
+    [HideInInspector]
     public float speed = 10f;
 
     public int startHealth = 100;
@@ -10,15 +13,12 @@ public class Enemy : MonoBehaviour {
 
     public GameObject deathEffect;
 
-    private Transform target;
-    private int wavepointIndex = 0;
-
     [Header("Unity Stuff")]
     public Image healthBar;
 
     void Start()
     {
-        target = Waypoints.points[0];
+        speed = startSpeed;
         health = startHealth;
     }
 
@@ -40,28 +40,6 @@ public class Enemy : MonoBehaviour {
         Destroy(effect, 2f);
 
         Destroy(gameObject);
-    }
-
-    void Update()
-    {
-        Vector3 dir = target.position - transform.position;
-        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
-
-        if (Vector3.Distance(transform.position, target.position) <= 0.4f )
-        {
-            GetNextWaypoint();
-        }
-    }
-
-    void GetNextWaypoint()
-    {
-        if(wavepointIndex >= Waypoints.points.Length - 1)
-        {
-            Destroy(gameObject);
-            return;
-        }
-        wavepointIndex++;
-        target = Waypoints.points[wavepointIndex];
     }
 
 }
