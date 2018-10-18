fdda23f65a2c009f31e0fd4f59a1a7de7709f501
JoeVRLab <chatelainj19@knoxr1.us>
2018-10-18 16:30:24 -0500

    Melee Created 10/18

---
+++ Assets/Scripts/DetectingObject.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class DetectingObject : MonoBehaviour {
+
+    private Transform target;
+    public int damage = 50;
+
+    public void Seek (Transform _target)
+    {
+        target = _target;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "EnemyFast(Clone)")
+        {
+            Debug.Log("Hit");
+            Destroy(col.gameObject);
+        }
+
+    }
+
+    void HitTarget()
+    {
+        Damage(target);
+
+        Destroy(gameObject);
+    }
+
+    void Damage(Transform enemy)
+    {
+        Enemy e = enemy.GetComponent<Enemy>();
+        if (e != null)
+        {
+            e.TakeDamage(damage);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Detector.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Detector : MonoBehaviour {
+
+    private Transform target;
+    public float range = 5f;
+
+    public GameObject bulletPrefab;
+
+    [Header("Unity Setup Fields")]
+
+    public string enemyTag = "Enemy";
+
+    //Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget ()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+}
