666ef54dea272bfcf11f2d4d92b9fdce4b91dc06
NickVRLab <scottn19@knoxr1.us>
2018-10-22 14:28:34 -0500

    Test to see if the shop menu closes

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -4,7 +4,12 @@ public class Shop : MonoBehaviour {
 
 	public void PurchaseStandardTurret ()
     {
+        Debug.Log("Standard Turret Purchased");
+    }
 
+    public void Close()
+    {
+        Destroy(gameObject);
     }
 
 }
