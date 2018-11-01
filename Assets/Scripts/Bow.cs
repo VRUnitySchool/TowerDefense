e808b3ff70f38ad6c62e8bd58386ed24d70d75d4
JoeVRLab <chatelainj19@knoxr1.us>
2018-11-01 15:16:44 -0500

    Final Push 11/1

---
+++ Assets/Scripts/Bow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bow : MonoBehaviour {
+    float _charge;
+    public float chargeMax;
+    public float chargeRate;
+    public KeyCode FireButton;
+    public Transform spawn;
+    public Rigidbody rigbArrow;
+
+    void Update()
+    {
+        if (Input.GetKey(FireButton) && _charge < chargeMax)
+        {
+            _charge += Time.deltaTime * chargeRate;
+            Debug.Log(_charge.ToString());
+        }
+
+        if (Input.GetKeyUp(FireButton))
+            {
+                Rigidbody arrow = Instantiate(rigbArrow, spawn.position, Quaternion.identity) as Rigidbody;
+                arrow.AddForce(spawn.forward * _charge, ForceMode.Impulse);
+            _charge = 0;
+            }
+    }
+}
