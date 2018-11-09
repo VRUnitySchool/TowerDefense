85fe9530a4788e989dd6acd354902cc1c26e874d
NickVRLab <scottn19@knoxr1.us>
2018-11-09 14:37:08 -0600

    MainMenu almost done

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public string levelToLoad = "TowerDefense";
+
+    public void Play ()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void Quit ()
+    {
+        Debug.Log("Quiting...");
+        Application.Quit();
+    }
+
+}
