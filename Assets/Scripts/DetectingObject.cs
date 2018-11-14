fc731f13a5f115c5f6710c05fcf75a7fb96d6c4b
JoeVRLab <chatelainj19@knoxr1.us>
2018-11-14 15:10:10 -0600

    Final Push 11/14

--- Assets/Scripts/DetectingObject.cs
+++ Assets/Scripts/DetectingObject.cs
@@ -2,34 +2,37 @@
 
 public class DetectingObject : MonoBehaviour {
 
-    private Enemy enemy;
-
-    private void Start()
-    {
-        enemy = GetComponent<Enemy>();
-    }
-
     void OnTriggerEnter(Collider col)
     {
+
         if (col.gameObject.name == "EnemyFast(Clone)")
         {
             Debug.Log("Hit");
-            enemy.health -= 90;
+            Damage(col.transform);
         }
         else if (col.gameObject.name == "EnemyNormal(Clone)")
         {
             Debug.Log("Hit");
-            enemy.health -= 90;
+            Damage(col.transform);
         }
         else if (col.gameObject.name == "EnemyStrong(Clone)")
         {
             Debug.Log("Hit");
-            enemy.health -= 90;
+            Damage(col.transform);
         }
 
     }
 
- 
+    void Damage(Transform enemy)
+    {
+        Enemy e = enemy.GetComponent<Enemy>();
+        if (e != null)
+        {
+            e.TakeDamage(20);
+        }
+
+    }
+
     private void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag == ("Floor"))
