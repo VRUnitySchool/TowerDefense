5988bb9a8ba9521e5208cbf3d1a43e7e0132a5eb
NickVRLab <scottn19@knoxr1.us>
2018-11-16 14:54:41 -0600

    Helathbar rotates but can't save the camera

---
+++ Assets/Scripts/HealthRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthRotation : MonoBehaviour {
+
+    public Camera myCamera;
+
+    private void Update()
+    {
+        transform.LookAt(transform.position + myCamera.transform.rotation * Vector3.back, myCamera.transform.rotation * Vector3.up);
+    }
+}
