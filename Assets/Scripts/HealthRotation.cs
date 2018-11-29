5d49333dd7184a6c338be42d8fa13bf030b02900
NickVRLab <scottn19@knoxr1.us>
2018-11-29 14:49:07 -0600

    Hopefully Good now

--- Assets/Scripts/HealthRotation.cs
+++ Assets/Scripts/HealthRotation.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthRotation : MonoBehaviour {
-
-    public Camera myCamera;
+public class HealthRotation : MonoBehaviour
+{
 
     private void Update()
     {
-        transform.LookAt(transform.position);
+        Camera MyCamera = Camera.main;
+        transform.LookAt(transform.position + MyCamera.transform.rotation * Vector3.back, MyCamera.transform.rotation * Vector3.up);
     }
 }
+
