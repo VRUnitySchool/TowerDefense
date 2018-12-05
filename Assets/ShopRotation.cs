4c9bbab1b89ce27c6c0304bd03596bade0cac378
NickVRLab <scottn19@knoxr1.us>
2018-12-05 14:42:49 -0600

    ShopRotation now fixed

---
+++ Assets/ShopRotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthRotation : MonoBehaviour
+{
+    float lockPos = 0;
+
+    private void Update()
+    {
+        Camera MyCamera = Camera.main;
+        transform.LookAt(transform.position + MyCamera.transform.rotation * Vector3.back, MyCamera.transform.rotation * Vector3.up);
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, lockPos, lockPos);
+    }
+}
