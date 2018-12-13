f82638c337cd6b258ab4ddf1229de1473c49bd10
NickVRLab <scottn19@knoxr1.us>
2018-12-13 14:16:45 -0600

    Starting push 12/13

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -1,13 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Shop : MonoBehaviour {
 
+    [HideInInspector]
     public Vector3 positionOffset;
-    public TurretBlueprint standardTurret;
     public GameObject node;
-    public GameObject turret;
     public GameObject image;
     public GameObject buildEffect;
+    public bool isUpgraded = false;
+
+    public TurretBlueprint standardTurret;
+    public Text upgradeCost;
+    public Button upgradeButton;
+    public TurretBlueprint turretBlueprint;
+    public GameObject turret;
+
+    private Node target;
+    private TurretBlueprint turretToBuild;
+
 
     BuildManager buildManager;
 
@@ -16,12 +27,22 @@ public class Shop : MonoBehaviour {
         buildManager = BuildManager.instance;
     }
 
+    public void SelectTurretToBuild(TurretBlueprint turret)
+    {
+        turretToBuild = turret;
+    }
+
     public void PurchaseStandardTurret ()
     {
         //Build Turret
         Debug.Log("Turret Selected");
         buildManager.SelectTurretToBuild(standardTurret);
-        Instantiate(turret, node.transform.position + positionOffset, Quaternion.identity);
+        Instantiate(turretBlueprint.prefab, node.transform.position + positionOffset, Quaternion.identity);
+
+        //Build a new one
+        GameObject _turret = (GameObject)Instantiate(standardTurret.upgradedPrefab, node.transform.position, Quaternion.identity);
+        turret = _turret;
+        
 
         //Play Build effect
         GameObject effect = Instantiate(buildEffect, node.transform.position, Quaternion.identity);
@@ -34,6 +55,24 @@ public class Shop : MonoBehaviour {
     public void Close()
     {
         image.SetActive(false);
+    }
+
+    public void UpgradeTurret()
+    {
+        if (PlayerStats.Money < turretBlueprint.upgradeCost)
+        {
+            Debug.Log("Not enough money to upgrade that!");
+            return;
+        }
+        PlayerStats.Money -= turretBlueprint.upgradeCost;
+
+        //Get rid of the old turret
+        Destroy(turret);
+        
+
+        isUpgraded = true;
+
+        Debug.Log("Turret upgraded!");
     }
 
 }
