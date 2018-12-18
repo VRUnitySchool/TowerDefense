733b127da88edfaed54ae07574d3659a6afca567
JoeVRLab <chatelainj19@knoxr1.us>
2018-12-18 14:43:51 -0600

    Revert "Merge branch 'master' of https://github.com/VRUnitySchool/TowerDefense"
    This reverts commit b91468f67f6461fbba686234fbbea7a82692c437, reversing
    changes made to f806158ac55ab2792e760e97b1d472d3c7725a64.

--- Assets/Scripts/SellTurret.cs
+++ Assets/Scripts/SellTurret.cs
@@ -1,16 +1,13 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class SellTurret : MonoBehaviour {
 
     public GameObject turret;
     public int value = 75;
 
-    public GameObject UI;
-
-    public Text sellText;
-
-    public void Sell()
+	public void Sell()
     {
         Destroy(turret);
         PlayerStats.Money += value;
---
+++ Assets/Upgrade.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Upgrade : MonoBehaviour {
+
+    public GameObject ui;
+
+    public Text upgradeCost;
+    public Button upgradeButton;
+
+}
